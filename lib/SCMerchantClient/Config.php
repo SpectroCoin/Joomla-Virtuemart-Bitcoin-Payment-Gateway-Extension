1e4b135e19f3b224b98288bd6172dba36977b648
SteinCode <rokas.student@gmail.com>
2025-02-18 08:58:46 +0200

    2.0.0

--- lib/SCMerchantClient/Config.php
+++ lib/SCMerchantClient/Config.php
@@ -8,9 +8,9 @@ defined('_JEXEC') or die('Restricted access');
 
 class Config
 {
-    const MERCHANT_API_URL = 'https://test.spectrocoin.com/api/public';
-    const AUTH_URL = 'https://test.spectrocoin.com/api/public/oauth/token';
-    const PUBLIC_SPECTROCOIN_CERT_LOCATION = 'https://test.spectrocoin.com/public.pem'; //PROD: https://spectrocoin.com/files/merchant.public.pem
+    const MERCHANT_API_URL = 'https://spectrocoin.com/api/public';
+    const AUTH_URL = 'https://spectrocoin.com/api/public/oauth/token';
+    const PUBLIC_SPECTROCOIN_CERT_LOCATION = 'https://spectrocoin.com/files/merchant.public.pem';
     const ACCEPTED_FIAT_CURRENCIES = ["EUR", "USD", "PLN", "CHF", "SEK", "GBP", "AUD", "CAD", "CZK", "DKK", "NOK"];
 
     // Joomla specific constants
